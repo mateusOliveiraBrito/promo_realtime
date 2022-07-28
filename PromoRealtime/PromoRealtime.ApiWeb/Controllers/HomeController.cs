309ebb0a934b5caae78b50e72930f17c1ad8dd9a
MATEUS OLIVEIRA BRITO <moliveirabrito.institucional@gmail.com>
2022-07-28 18:56:58 -0300

    Cria estrutura inicial da solução e do projeto

---
+++ PromoRealtime/PromoRealtime.ApiWeb/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PromoRealtime.ApiWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromoRealtime.ApiWeb.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
