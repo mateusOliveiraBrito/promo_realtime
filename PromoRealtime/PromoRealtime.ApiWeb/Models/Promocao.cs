55fd81b99127c479a62a2f6f0b50e296752b59ad
MATEUS OLIVEIRA BRITO <moliveirabrito.institucional@gmail.com>
2022-07-28 21:09:14 -0300

    Implementa model de promoção

---
+++ PromoRealtime/PromoRealtime.ApiWeb/Models/Promocao.cs
@@ -0,0 +1,8 @@
+﻿namespace PromoRealtime.ApiWeb.Models {
+    public class Promocao {
+        public string Empresa { get; set; }
+        public string Chamada { get; set; }
+        public string Regras { get; set; }
+        public string EnderecoURL { get; set; }
+    }
+}
