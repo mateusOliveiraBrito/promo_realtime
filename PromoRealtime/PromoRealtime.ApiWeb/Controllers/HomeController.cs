430c9a65f7525f83b91f83ed601d8646b201e784
MATEUS OLIVEIRA BRITO <moliveirabrito.institucional@gmail.com>
2022-07-29 09:57:15 -0300

    Implementa rotas para acesso às páginas

--- PromoRealtime/PromoRealtime.ApiWeb/Controllers/HomeController.cs
+++ PromoRealtime/PromoRealtime.ApiWeb/Controllers/HomeController.cs
@@ -3,5 +3,16 @@
 namespace PromoRealtime.ApiWeb.Controllers {
     public class HomeController : Controller {
 
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult Promocao() {
+            return View();
+        }
+
+        public ActionResult CadastrarPromocao() {
+            return View();
+        }
     }
 }
