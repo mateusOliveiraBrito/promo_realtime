bcab97efd2ea4012d4f190d9615610d1b108445f
MATEUS OLIVEIRA BRITO <moliveirabrito.institucional@gmail.com>
2022-07-28 21:09:02 -0300

    Implementa método para representar o cadastro de promoções

--- PromoRealtime/PromoRealtime.ApiWeb/Hubs/PromoHub.cs
+++ PromoRealtime/PromoRealtime.ApiWeb/Hubs/PromoHub.cs
@@ -1,6 +1,13 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using PromoRealtime.ApiWeb.Models;
+using System.Threading.Tasks;
 
 namespace PromoRealtime.ApiWeb.Hubs {
     public class PromoHub : Hub {
+
+        public async Task CadastrarPromocao(Promocao promocao) {
+            await Clients.Caller.SendAsync("CadastradoSucesso");
+            await Clients.Others.SendAsync("ReceberPromocao", promocao);
+        }
     }
 }
